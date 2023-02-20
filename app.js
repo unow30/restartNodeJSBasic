8c03d076c8121d9e8a7edf3b108659c4747a58cc
unow30 <yunho7205@gmail.com>
2023-02-20 21:38:56 +0900

    express github code 확인

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
@@ -11,8 +11,23 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In other middleware');
+    
+    //유틸리티 함수 send
+    res.send('<h1>hello express</h1>');
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+// server.listen(3000);
 
-server.listen(3000);
+//더 짧은 방법
+//lib > application.js 확인
+app.listen(3000);
+
+
+//lib > application.js 확인
+// app.listen = function listen(){
+// 	var server = http.createServer(this);
+// 	return server.listen.apply(server, arguments);
+// }
+
+//http를 이미 실행해주고 있다. 위에 임포트한 http는 필요없어졌다.
