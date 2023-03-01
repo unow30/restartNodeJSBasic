831be204aa8dbbfaf69ae95db8890981d5d94061
unow30 <yunho7205@gmail.com>
2023-03-01 10:22:40 +0900

    util함수 생성. rootDir경로 찾기

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,19 @@
-const path = require('path');
-
 const express = require('express');
+
+const findRoute = require('../util/rootDir')
 
 const router = express.Router();
 
-//app.js에서 admin/을 라우터 경로로 추가하였다.
-//이제 {주소}/admin/add-product get 요청시 아래의 라우트가 실행하게 된다.
+//__dirname대신 path모듈을 활용한 util/path.js를 만들어 활용한다.
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));//rootDir에도 path를 사용하고 있어 낭비
+    res.sendFile(findRoute('views','add-product.html'));
 });
 
 //이제 {주소}/admin/add-product post 요청시 아래의 라우트가 실행하게 된다.
 router.post('/add-product', (req, res)=>{
     console.log(req.body);
-    res.redirect('/');
+    res.redirect('/shop');
 });
 
 module.exports = router;
---
+++ util/rootDir.js
@@ -0,0 +1,22 @@
+const path = require('path')
+// @deprecated — Since v14.0.0 - Use main instead.
+// 'mainModule'은(는) 사용되지 않습니다.ts(6385)
+// module.exports = path.dirname(process.mainModule); 
+
+// require.main.filename은 현재 Node.js 애플리케이션의 진입 모듈(main module)의 파일 경로를 반환합니다. (app.js)
+// path.dirname()은 파일 경로에서 디렉토리 경로만 추출하는 Node.js 내장 모듈 함수입니다. 
+// 따라서 path.dirname(require.main.filename)은 현재 Node.js 애플리케이션의 진입 모듈(main module)이 위치한 디렉토리 경로를 반환합니다.
+
+// root폴더, 다음 폴더, 파일 경로
+const findRoute = (folderName, fileName) => {
+
+    // if(typeof folderName !== 'string' || typeof fileName !== 'string'){
+    //     console.log('its not match folderName|fileName');
+    // }
+
+    const rootDir = path.dirname(require.main.filename);
+    console.log(rootDir)
+    return path.join(rootDir, folderName, fileName)
+}
+
+module.exports = findRoute
